8bb9f7766f9d0fe79f8c6350ffb33e0699f7045e
Anton Genchev <antongenchevfractal@gmail.com>
2024-10-10 18:31:33 +0300

    Add buttons for taking and closing a screenshot

--- src/ScreenshotApp.py
+++ src/ScreenshotApp.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 
 class ScreenshotApp(QWidget):
     def __init__(self):
@@ -11,4 +11,22 @@ class ScreenshotApp(QWidget):
         '''
         self.layout = QVBoxLayout()
         self.setWindowTitle('Screenshot Taker')
-        self.setGeometry(2920, 300, 400, 400)
+        self.setGeometry(2920, 300, 400, 400) # Where to start the app: position (2930, 300) on the screen
+
+        h_layout = QHBoxLayout()
+        self.button_screenshot = QPushButton('Take a new screenshot', self)
+        self.button_screenshot.clicked.connect(self.on_take_screenshot)
+        self.button_close_screenshot = QPushButton('Close screenshot', self)
+        self.button_close_screenshot.clicked.connect(self.on_close_screenshot)
+        h_layout.addWidget(self.button_screenshot)
+        h_layout.addWidget(self.button_close_screenshot)
+        self.layout.addLayout(h_layout)
+
+        self.setLayout(self.layout)
+
+    def on_take_screenshot(self):
+        print('Take screenshot')
+
+
+    def on_close_screenshot(self):
+        print('Close screenshot')
