ffcfc1d6bb0d04758409c960e7e94df7d785dd82
Anton Genchev <antongenchevfractal@gmail.com>
2024-10-17 20:08:39 +0300

    Fix handling of events originating from DraggableBox.
    -Refactor event handling (mouseMoveEvent, mouseReleaseEvent, mousePressEvent and eventFilter) to correctly handle the positions of events coming from draggable_widget
    -Refactor creation and deletion of draggable_widget. Keep the draggable_widget so that the moving events are registered

--- src/TransparentWindow.py
+++ src/TransparentWindow.py
@@ -35,29 +35,53 @@ class TransparentWindow(QMainWindow):
         self.screenshot_widget.setStyleSheet(f"background-image: url('{config['paths']['screenshot_background']}'); background-repeat: no-repeat;")
         self.setCentralWidget(self.screenshot_widget)
 
-    def mousePressEvent(self, event):
+    def mousePressEvent(self, event, event_pos=None):
+        '''
+        Handle the mouse press event.
+        ctrl + left click => start drawing a rectangle/selection/DraggableBox
+
+        Parameters:
+            event: the Qt event
+            event_pos: usually None. If the event is coming from outside the TransparentWindow make it relative to the coordinates of Transp.Window
+        '''
         if event.button() == Qt.LeftButton:
             if QApplication.keyboardModifiers() == Qt.ControlModifier:
-                self.start_pos = event.pos() # The first corner of the box
-                self.overlay.start_pos = event.pos()
+                event_pos = event.pos() if event_pos is None else event_pos
+                self.start_pos = event_pos # The first corner of the box
+                self.overlay.start_pos = event_pos
                 self.is_drawing = True
                 self.overlay.is_drawing = True
 
                 # Destory the existing draggable widget if it exists
                 if self.draggable_widget:
-                    self.draggable_widget.deleteLater()
-                    self.draggable_widget = None
+                    # self.draggable_widget.deleteLater()
+                    # self.draggable_widget = None
+                    pass
 
-    def mouseMoveEvent(self, event):
+    def mouseMoveEvent(self, event, event_pos=None):
+        '''
+        Handle mouse movements
+        self.is_drawing => change the rectangle being drawn
+
+        Parameters:
+            event: the Qt event
+            event_pos: usually None. If the event is coming from outside the TransparentWindow make it relative to the coordinates of Transp.Window
+        '''
         if self.is_drawing:
-            self.end_pos = event.pos() # Track the other corner of the box
+            event_pos = event.pos() if event_pos is None else event_pos
+            self.end_pos = event_pos # Track the other corner of the box
             self.overlay.end_pos = self.end_pos
             self.overlay.update()
 
-    def mouseReleaseEvent(self, event):
+    def mouseReleaseEvent(self, event, event_pos=None):
+        '''
+        Handle mouse release events
+        self.is_drawing and left release => Make a new DraggableBox
+        '''
         if event.button() == Qt.LeftButton and self.is_drawing:
-            self.end_pos = event.pos()
-            self.overlay.end_pos = event.pos()
+            event_pos = event.pos() if event_pos is None else event_pos
+            self.end_pos = event_pos
+            self.overlay.end_pos = event_pos
             # Create the new draggable widget
             if self.start_pos and self.end_pos:
                 self.create_draggable_widget()
@@ -66,14 +90,22 @@ class TransparentWindow(QMainWindow):
 
     def eventFilter(self, obj, event):
         if obj == self.draggable_widget:
-            if QApplication.keyboardModifiers() == Qt.ControlModifier:
-                if event.type() in [QEvent.MouseButtonPress, QEvent.MouseMove, QEvent.MouseButtonRelease]:
-                    self.mousePressEvent(event)
-                    return True
+            if QApplication.keyboardModifiers() == Qt.ControlModifier or self.is_drawing:
+                print('Event Filter. Event type: ', event.type()==QEvent.MouseButtonRelease)
+                if event.type() == QEvent.MouseButtonPress:
+                    event_pos = self.mapFromGlobal(obj.mapToGlobal(event.pos()))
+                    self.mousePressEvent(event, event_pos=event_pos)
+                elif event.type() == QEvent.MouseMove:
+                    event_pos = self.mapFromGlobal(obj.mapToGlobal(event.pos()))
+                    self.mouseMoveEvent(event, event_pos=event_pos)
+                elif event.type() == QEvent.MouseButtonRelease:
+                    event_pos = self.mapFromGlobal(obj.mapToGlobal(event.pos()))
+                    self.mouseReleaseEvent(event, event_pos=event_pos)
+                return True
         return super().eventFilter(obj, event)
 
     def create_draggable_widget(self):
-        print(self.start_pos, self.end_pos)
+        # Calculate the position and size of the new DraggableBox
         left = min(self.start_pos.x(), self.end_pos.x())
         top = min(self.start_pos.y(), self.end_pos.y())
         width = abs(self.start_pos.x() - self.end_pos.x())
@@ -81,7 +113,13 @@ class TransparentWindow(QMainWindow):
         if width == 0 or height == 0:
             # Can not create a DraggableBox with size 
             return None
+
         selection = Box(left, top, width, height)
-        self.draggable_widget = DraggableBox(self, selection=selection)
-        self.draggable_widget.installEventFilter(self)
-        self.draggable_widget.show()
+        if self.draggable_widget:
+            # Change the selection of the existing DraggableBox
+            self.draggable_widget.on_change_selection(selection)
+        else:
+            # Create the new DraggableBox
+            self.draggable_widget = DraggableBox(self, selection=selection)
+            self.draggable_widget.installEventFilter(self)
+            self.draggable_widget.show()
