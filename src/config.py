2435dcf755ffa3a0ccfe39c285ecb549893c7167
Anton Genchev <antongenchevfractal@gmail.com>
2024-10-18 17:38:17 +0300

    Prepare the project for compiling and building using pyinstaller. Refactor config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,13 @@
 import json
+import os
+import sys
 
-with open('config.json', 'r') as config_file:
+if getattr(sys, 'frozen', False):
+    # If the application is bundled, use the path to the temporary folder
+    config_path = os.path.join(sys._MEIPASS, 'config.json')
+else:
+    # If running in a normal Python environment, use the local path
+    config_path = 'config.json'
+
+with open(config_path, 'r') as config_file:
     config = json.load(config_file)
