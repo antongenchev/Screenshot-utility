4e1722007ebc90d699f7162d8cc7bd58c906561d
Anton Genchev <antongenchevfractal@gmail.com>
2024-10-24 20:41:48 +0300

    Extend the Memento class by adding is_related, and attributes _timestamp_created, _source

--- src/Memento.py
+++ src/Memento.py
@@ -1,7 +1,30 @@
+import time
+from typing import Any, Optional, Dict
 from src.utils import Box
+from src.config import config
 
 class Memento:
-    def __init__(self, **kwargs):
+    def __init__(self,  _source:Optional[str]=None, **kwargs,):
+        # Store the data
         for key, value in kwargs.items():
-            self.__setattr__(key, value)
-    
+            if key.startswith('_'):
+                raise Exception(f'Not allowed for keys to start with an underscored: {key}')
+            setattr(self, key, value)
+
+        # Save the timestamp of when the memento was created
+        self._timestamp_created = time.time()
+
+        self._source = _source # indicate the source of the memento creation
+
+    def is_related(self, m:'Memento') -> bool:
+        '''
+        Check if the memento m is related to the current memento.
+        This function can be overriden in more concrete implementations of Mementos
+
+        Parameters:
+            m: the other memento object to compare with
+        Returns:
+            (bool) whether the two mementos are considered related
+        '''
+        return False
+
